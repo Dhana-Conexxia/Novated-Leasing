75d2269853464077d4a8e4ae660b4943a707308d
Rachel Kitchen <rachel.kitchen@appian.com>
2025-06-05 12:45:23 -0400

    Remove handling for local drivers in setup scripts
    We no longer keep drivers in the source code, so the driver copying/handling is deprecated

--- fitnesse-for-appian/src/main/resources/setupCustomProperties.sh
+++ fitnesse-for-appian/src/main/resources/setupCustomProperties.sh
@@ -4,7 +4,6 @@ CURRENT_DIR=`dirname $0`
 
 TARGET=$(realpath "./")
 RESOURCES="$TARGET"
-DRIVERS="$RESOURCES/lib/drivers"
 CUSTOM_PROPERTIES="$RESOURCES/configs/custom.properties"
 
 sed-populate-key(){
@@ -13,5 +12,3 @@ sed-populate-key(){
 
 sed-populate-key "automated.testing.home" "$RESOURCES"
 sed-populate-key "download.directory" "$TARGET"
-sed-populate-key "chrome.driver.home" "$DRIVERS/chromedriver-linux"
-sed-populate-key "firefox.driver.home" "$DRIVERS/geckodriver-linux64"
--- shared/setupCustomPropertiesForLinux.sh
+++ shared/setupCustomPropertiesForLinux.sh
@@ -6,7 +6,6 @@ RESOURCES=$(realpath $CURRENT_DIR)
 echo "Setting resources to be $RESOURCES"
 
 TARGET=$(realpath "$CURRENT_DIR/target")
-DRIVERS="$RESOURCES/lib/drivers"
 CUSTOM_PROPERTIES="$RESOURCES/configs/custom.properties"
 
 sed-populate-key() {
@@ -15,5 +14,3 @@ sed-populate-key() {
 
 sed-populate-key "automated.testing.home" "$RESOURCES"
 sed-populate-key "download.directory" "$TARGET"
-sed-populate-key "chrome.driver.home" "$DRIVERS/chromedriver-linux"
-sed-populate-key "firefox.driver.home" "$DRIVERS/geckodriver-linux64"
